39705f1afec892559dbe07d6aec6c4116ea2e8d2
Ben Tyler <benjamin.tyler@gmail.com>
2015-06-19 23:13:41 +0200

    Clean up IRC interface; add logging; debug.
    Also a tiny bit of doc, and a simple user echo bot.

--- lib/Logbot.pm
+++ lib/Logbot.pm
@@ -3,7 +3,6 @@ use Mojo::Base 'Mojolicious';
 use Logbot::IRC;
 use experimental qw(postderef signatures);
 
-# This method will run once at server start
 sub startup ($c) {
 	my $config = $c->plugin('Config');
 
@@ -11,11 +10,16 @@ sub startup ($c) {
 
 	$c->helper(irc => sub {
 		state $irc = Logbot::IRC->new(
+			channels => $config->{irc}->{channels},
 			user => $config->{irc}->{user},
 			nick => $config->{irc}->{nick},
 			server => $config->{irc}->{server},
 			pass => $config->{irc}->{pass},
 		);
+	});
+
+	$c->irc->on(message => sub ($irc, $channel, $user, $message) {
+		$irc->say($user => "hi $user. did you say $message?");
 	});
 
 	$c->irc->connect;
--- lib/Logbot/IRC.pm
+++ lib/Logbot/IRC.pm
@@ -1,42 +1,64 @@
 package Logbot::IRC;
 use Mojo::Base 'Mojo::IRC';
 use Mojo::IOLoop;
+use Mojo::IOLoop::Delay;
+use Mojo::Log;
+
 use experimental qw(postderef signatures);
+use Data::Dumper qw(Dumper);
+
+use constant DEBUG => $ENV{LOGBOT_DEBUG} ? 1 : 0;
+
+has channels => sub { [ '#blorgbot' ] };
+has log => sub { state $irc = Mojo::Log->new };
+has polite_delay => sub { 2 };
 
 sub new {
 	my $self = shift->SUPER::new(@_);
 
-	$self->register_default_event_handlers;
-
 	$self->on(irc_join => sub ($, $msg) {
-		say "yay! i joined $msg->{params}[0]";
-		$self->write(PRIVMSG => '#blorgbot', 'blorg', sub ($, $err) {
-			say $err if $err;
-			say "finished blorging";
-		});
+		$self->emit(joined => $msg->{params}[0]);
 	});
 
 	$self->on(irc_privmsg => sub ($, $msg) {
-		say $msg->{prefix}, " said: ", $msg->{params}[1];
-		Mojo::IOLoop->timer(1 => sub {
-			$self->write(PRIVMSG => '#blorgbot', ":you said: ", $msg->{params}[1]);
-		});
+		# message->{prefix} looks like this:
+		# FoobarUser!US_0@1799.lobby.springrts.com
+		my @prefix = split/!/, $msg->{prefix};
+		my $user = shift @prefix;
+		my ($channel, $message) = $msg->{params}->@[0, 1];
+
+		warn "$channel >>>>> $user: $message\n" if DEBUG;
+
+		$self->emit(message => $channel, $user, $message);
 	});
 
 	$self->on(error => sub ($, $err) {
-		say "bad; $err";
+		$self->log->warn("error in IRC connection: $err");
 	});
 
 	$self->on(close => sub ($, $message) {
-		Mojo::IOLoop->timer(2 => sub {
+		Mojo::IOLoop->timer($self->polite_delay => sub {
 			$self->connect;
 		});
 	});
 
 	$self->on(irc_rpl_welcome => sub ($, $params) {
-		Mojo::IOLoop->timer(2 => sub {
-			$self->write(join => '#blorgbot');
-		});
+		Mojo::IOLoop->delay(
+			sub ($d) {
+				Mojo::IOLoop->timer($self->polite_delay => $d->begin);
+			},
+			sub ($d) {
+				for my $chan ($self->channels->@*) {
+					$self->join($chan, $d->begin);
+				}
+			},
+			sub ($d, @write_errs) {
+				die @write_errs if grep { $_ } @write_errs;
+				$self->emit('joined_all_channels');
+			}
+		)->catch(sub ($d, @errs) {
+			$self->log->error("errors joining channels: " . join "\n", @errs);
+		})->wait;
 	});
 
 	return $self;
@@ -44,10 +66,48 @@ sub new {
 
 sub connect ($self, $cb = '') {
 	$cb = ref $cb eq 'CODE' ? $cb : sub {
-		say "the bot has connected!";
+		$self->log->info("logbot has connected to " . $self->server);
 	};
+
+	$self->register_default_event_handlers;
 
 	$self->SUPER::connect($cb);
 }
 
+sub join ($self, $chan, $cb) {
+	$self->write(join => $chan, $cb);
+}
+
+sub say ($self, $channel, $message, $cb = sub { }) {
+	warn "$channel <<<<< $message\n" if DEBUG;
+
+	$self->write(PRIVMSG => $channel, ":$message", $cb);
+}
+
 1;
+
+=head1 NAME
+
+Logbot::IRC -- wrapper around Mojo::IRC for the notification/logbot
+
+=head1 SYNOPSIS
+
+	my $irc = Logbot::IRC->new(
+		channels => [qw( #foo #bar #baz) ],
+		nick => '[S44]BlorgBot',
+		user => '[S44]BlorgBot',
+		pass => 'something_secret',
+		server => 'irc.springrts.com:6667',
+	);
+
+	$irc->connect;
+	# echo bot in all active channels
+	$irc->on(message => sub ($, $channel, $user, $message) {
+		$irc->say($channel => "$user says $message!");
+	});
+
+=head1 DESCRIPTION
+
+	This transforms Mojo::IRC events into a slightly higher level interface.
+
+=cut
