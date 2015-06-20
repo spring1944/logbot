b073501a9f53cdd4c1e8653a32346c3d58cf91d4
BT <bt@nemo>
2015-06-19 21:23:15 -0500

    Add handler for issue comment.
    Lots of duplicated code at the moment, and more on the horizon: need to factor
    out the short link fetching.

--- lib/Logbot/Github.pm
+++ lib/Logbot/Github.pm
@@ -18,22 +18,51 @@ my %handlers = (
 	push => sub ($p, $cb) {
 
 	},
+	issue_comment => sub ($p, $cb) {
+		my $comment = $p->{comment};
+		my $issue = $p->{issue};
+		Mojo::IOLoop->delay(
+			sub ($d) {
+				die "no comment data" if !$comment;
+
+				$ua->post('http://git.io' => form => { url => $comment->{html_url} } => $d->begin);
+			},
+			sub ($d, $tx) {
+				die $tx->error if $tx->error;
+				my $link = $tx->res->headers->location // $comment->{html_url};
+				my $preview = substr $comment->{body}, 0, 60;
+				if (length $preview < length $comment->{body}) {
+					$preview .= '...';
+				}
+				$preview =~ s/^\s+//g;
+				$preview =~ s/\s+$//g;
+				my $res = "$p->{sender}->{login} commented on $issue->{title} (#$issue->{number}): $preview. $link";
+				$cb->($d, $res);
+			}
+		)->catch( sub ($d, $err) {
+			say "error blorg in github; $err";
+			$cb->($d, '', $err);
+		})->wait;
+	},
+
 	issues => sub ($p, $cb) {
 		my $issue = $p->{issue};
 		Mojo::IOLoop->delay(
 			sub ($d) {
 				die "no issue data" if !$issue;
 
-				$ua->post('http://git.io' => form => { url => $issue->{url} } => $d->begin);
+				$ua->post('http://git.io' => form => { url => $issue->{html_url} } => $d->begin);
 			},
-			sub ($d, $ua, $tx) {
+			sub ($d, $tx) {
 				die $tx->error if $tx->error;
-				my $link = $tx->res->headers->location // $issue->{url};
+				my $link = $tx->res->headers->location // $issue->{html_url};
 				# 'kanatohodets opened #43: airplanes fly oddly. review: git.io/asdfds'
-				$cb->("$p->{sender}->{login} $p->{action} #$issue->{number}: $issue->{title}. review: $link");
+				my $res = "$p->{sender}->{login} $p->{action} #$issue->{number}: $issue->{title}. review: $link";
+				$cb->($d, $res);
 			}
 		)->catch( sub ($d, $err) {
-			$cb->('', $err);
+			say "error blorg in github; $err";
+			$cb->($d, '', $err);
 		})->wait;
 	},
 );
